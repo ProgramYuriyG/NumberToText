9b7cdd1f823f25781113a140206e2e780fea5a6d
Yuriy Glukhov <36609813+ProgramYuriyG@users.noreply.github.com>
2018-03-12 22:06:34 -0400

    NumberToText.java 2.0
    Created a new method to count which performs all actions for numbers greater than single digits. Currently changed up to 99. Working on fixes for higher numbers.

--- NumberToText.java
+++ NumberToText.java
@@ -1,4 +1,3 @@
-import java.math.BigInteger;
 import java.util.*;
 public class NumberToText {
 
@@ -9,75 +8,70 @@ public static String [] tensNames = {"ten","eleven","twelve","thirteen","fourtee
 public static String [] digitsNames = {"","","","hundred","thousand","million","billion","trillion","quadrillion","quintillion"};
 
     //Main Method
-    public static void main(String[]args){
+    public static void main(String[]args) {
         Scanner input = new Scanner(System.in);
 
         //Asks user for input
         System.out.println("What interger number do you wish to convert?");
-        int numberInput = input.nextInt();
+        double numberInput = input.nextDouble();
 
         //does a variable to check the double place of numberInput
-        int doubleNumber = numberInput/10;
-        int threeNumber = numberInput/100;
+        double doubleNumber = numberInput / 10;
+        double threeNumber = numberInput / 100;
+        double fourNumber = numberInput / 1000;
+        double fiveNumber = numberInput / 10000;
 
         //makes the input into a string
-        String numberInputString = numberInput +"";
-        int digitLength = numberInputString.length();
+        String numberInputString = numberInput + "";
+        int digitLength = numberInputString.length() - 2;
 
         //calls the correct method
-            //if the number is greater than 2 digits
-        if(numberInputString.length() > 2){
-        System.out.print(singleDigitNames[threeNumber]);
-        System.out.print(" " + digitsNames[digitLength]+" ");
-        DoubleDigit(numberInput%100);
-        System.out.print(" ");
-        SingleDigit(numberInput%10);
-
-        }
-            //if the number is double digit
-        if(numberInputString.length() == 2){
-            DoubleDigit(numberInput);
-            System.out.print(" ");
-            if(doubleNumber == 1) {
-            }else{
-                SingleDigit(numberInput);
-            }
-        }
-            //if the number is single digit
-        if( numberInputString.length() == 1 ){
-            SingleDigit(numberInput);
-        }
-
-    }
-
-    //Class for returning the single digit
-    public static void SingleDigit(int numberInput){
-        //makes a for loop to check what number it equals
-        for(int i = 0; i<=numberInput; i++){
-            if(i==(numberInput%10)){
-                System.out.print( singleDigitNames[i] );
-                break;
-            }
-        }
-
-    }
-
-    //Class for returning the double digit
-    public static void DoubleDigit(int numberInput){
-        //makes a for loop to check what number it equals
-        for(int i = 0; i<=numberInput; i++){
-            if(i==(numberInput/10)){
-                if( i== 1){
-                    i = numberInput%10;
-                    System.out.print(tensNames[i]);
+        int y = 0;
+            for (int x = digitLength; x != 0; x--) {
+                if (x == 1 && y == 0) {
+                    y = SingleDigit(numberInput);
                     break;
-                }else {
-                    System.out.print(doubleDigitNames[i]);
-                    break;
+                } else {
+                    y = AnyDigit(numberInput, x);
                 }
             }
         }
 
+
+    //Class for returning the single digit
+    public static int SingleDigit(double numberInput){
+        //makes a for loop to check what number it equals
+        for(int i = 0; i<=numberInput; i++){
+            if( i == (numberInput%10) ){
+                System.out.print( singleDigitNames[i] );
+                return 1;
+            }
+        }
+        return 0;
     }
 
+    //Class for returning the greater than 2 digit
+    public static int AnyDigit(double numberInput,int digitLength){
+        //makes a for loop to check what number it equals
+        for(int i = 0; i<=numberInput; i++){
+            if( i == (int)( numberInput/(Math.pow(10,digitLength-1)) ) ){
+                if( i == 1 && digitLength == 2){
+                    i = (int)numberInput%10;
+                    System.out.print(tensNames[i]+" ");
+                        return 1;
+                }else if( digitLength == 2 ){
+                    System.out.print(doubleDigitNames[i]+" ");
+                    if(numberInput%10==0) {
+                        return 1;
+                    }else{
+                        return 0;
+                    }
+                }else if(digitLength == 3){
+                    System.out.print(singleDigitNames[i]+"  hundred ");
+                    return 0;
+                }
+            }
+        }
+            return 0;
+    }
 }
